a8e441278260e3604752e3dc8f36797ad7f6ebfb
tanqiangyes <826285820@qq.com>
2022-11-03 16:59:44 +0800

    Design Process

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # calculator
+
+## step
+
+1. if we receive a stop signal, we stop processing, and return.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+//! 解析流程：
+//! 1、我们应该判断是否输入了终止条件；有的话，直接返回；
+//! 2、逐字节读取，判断token，直到结尾；
+//! 3、将token转换为node，同时应用运算符优先级规则
+//! 4、输出结果。
+use std::io::Stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buf = String::new();
+    let std_in: Stdin = std::io::stdin();
+    while std_in.read_line(&mut buf).is_ok() {
+        pure_buf = buf.trim_end();
+        // if receive stop signal, stop it.
+        if "bye" == pure_buf {
+            println!("bye");
+            break;
+        }
+
+        // change buf as bytes, so we can convert it into tokens.
+        let text = pure_buf.trim_end();
+        // let c: char = match text.chars().next() {
+        //     Some(next_char) => next_char,
+        //     None => {
+        //         return;
+        //         // return Ok((Tok::EOF, 0));
+        //     }
+        // };
+        // let () =  match c {
+        //
+        // }
+        println!("{:?}", text);
+        buf.clear();
+    }
 }
